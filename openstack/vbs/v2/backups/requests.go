8c7de392ddfcb900993e96c8e79ef1542179a98e
Sapan <sapan.vaswani@click2cloud.net>
2018-09-18 13:19:09 +0530

    add vbs backup get func comment

--- openstack/vbs/v2/backups/requests.go
+++ openstack/vbs/v2/backups/requests.go
@@ -159,7 +159,9 @@ func CreateBackupRestore(c *golangsdk.ServiceClient, id string, opts RestoreOpts
 	return
 }
 
-// Get retrieves a particular backup based on its unique ID.
+// Get retrieves a particular backup based on its unique ID. To extract
+//// the Backup object from the response, call the Extract method on the
+//// GetResult.
 func Get(c *golangsdk.ServiceClient, id string) (r GetResult) {
 	_, r.Err = c.Get(resourceURL(c, id), &r.Body, nil)
 	return
